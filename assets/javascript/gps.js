fbeec258b774af5782e3c42647d2b5a02fd812d1
KeiranHamilton <43836404+KeiranHamilton@users.noreply.github.com>
2018-12-06 23:20:12 -0500

    Update gps.js

--- assets/javascript/gps.js
+++ assets/javascript/gps.js
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Project 1</title>
-    <meta name="viewport" content="initial-scale=1.0" />
-    <meta charset="utf-8" />
-    <style>
+<style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
       #map {
@@ -44,7 +38,4 @@
 
       
     </script>
-  
-</body>
-</html>
 
